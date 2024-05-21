1d2c738e66d52fe14665d9554dafee830d2d636d
EtiNL <etienne.lanzeray@gmail.com>
2024-05-21 08:01:59 +0200

    moved build_attn_mask to transformer.py

--- CLGP.py
+++ CLGP.py
@@ -17,55 +17,15 @@ class LayerNorm(nn.LayerNorm):
         return ret.type(orig_type)
 
 
-class QuickGELU(nn.Module):
-    def forward(self, x: torch.Tensor):
-        return x * torch.sigmoid(1.702 * x)
-
-
-class ResidualAttentionBlock(nn.Module):
-    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
-        super().__init__()
-
-        self.attn = nn.MultiheadAttention(d_model, n_head)
-        self.ln_1 = LayerNorm(d_model)
-        self.mlp = nn.Sequential(OrderedDict([
-            ("c_fc", nn.Linear(d_model, d_model * 4)),
-            ("gelu", QuickGELU()),
-            ("c_proj", nn.Linear(d_model * 4, d_model))
-        ]))
-        self.ln_2 = LayerNorm(d_model)
-        self.attn_mask = attn_mask
-
-    def attention(self, x: torch.Tensor):
-        self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
-        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
-
-    def forward(self, x: torch.Tensor):
-        x = x + self.attention(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
-        return x
-
-
-class Transformer(nn.Module):
-    def __init__(self, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None):
-        super().__init__()
-        self.width = width
-        self.layers = layers
-        self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers)])
-
-    def forward(self, x: torch.Tensor):
-        return self.resblocks(x)
-
-
-class CLIP(nn.Module):
+class CLGP(nn.Module):
     def __init__(self,
                  embed_dim: int,
                  # graph
-                 image_resolution: int,
-                 vision_layers: Union[Tuple[int, int, int, int], int],
-                 vision_width: int,
-                 vision_patch_size: int,
+                 graph_encoder,
+                 graph_encoder_config,
                  # text
+                 text_encoder,
+                 text_encoder_config,
                  context_length: int,
                  vocab_size: int,
                  transformer_width: int,
@@ -76,9 +36,9 @@ class CLIP(nn.Module):
 
         self.context_length = context_length
 
-        self.gnn = GNN()
+        self.gnn = graph_encoder(graph_encoder_config)
 
-        self.transformer = Transformer(
+        self.transformer = text_encoder(
             width=transformer_width,
             layers=transformer_layers,
             heads=transformer_heads,
@@ -99,8 +59,9 @@ class CLIP(nn.Module):
         nn.init.normal_(self.token_embedding.weight, std=0.02)
         nn.init.normal_(self.positional_embedding, std=0.01)
 
-        #initialise GNN
+        #initialise graph_encoder
 
+        #initialise text_encoder
         proj_std = (self.transformer.width ** -0.5) * ((2 * self.transformer.layers) ** -0.5)
         attn_std = self.transformer.width ** -0.5
         fc_std = (2 * self.transformer.width) ** -0.5
@@ -113,13 +74,6 @@ class CLIP(nn.Module):
         if self.text_projection is not None:
             nn.init.normal_(self.text_projection, std=self.transformer.width ** -0.5)
 
-    def build_attention_mask(self):
-        # lazily create causal attention mask, with full attention between the vision tokens
-        # pytorch uses additive attention mask; fill with -inf
-        mask = torch.empty(self.context_length, self.context_length)
-        mask.fill_(float("-inf"))
-        mask.triu_(1)  # zero out the lower diagonal
-        return mask
 
     @property
     def dtype(self):
