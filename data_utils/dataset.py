9b86af9f8843e3d509b7b22c1ccc89121a1a73d7
EtiNL <etienne.lanzeray@gmail.com>
2024-06-11 17:08:09 +0200

    modified:   data_utils/dataset.py 	new file:   data_utils/graph_tokenizer_1.json 	new file:   data_utils/prompt_tokenizer_1.json 	new file:   data_utils/train_tokenizers.py 	deleted:    data_utils/unigram_tokenizer.json 	modified:   graph_question_links_3d.html

--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -8,38 +8,39 @@ from torch_geometric.data import Data
 class CLGP_Ebiose_dataset(Dataset):
     """CLGP_Ebiose_dataset. To train CLGP on prompt-graphs pairs."""
 
-    def __init__(self, config, prompt_tokenizer, graph_feature_tokenizer, context_length=77, input_resolution=224):
+    def __init__(self, config, prompt_tokenizer, graph_feature_tokenizer, prompt_context_length=100, node_feature_context_length = 100, mode = "training"):
         super(CLGP_Ebiose_dataset, self).__init__()
 
         self.config = config
-        self.context_length = context_length
+        self.prompt_context_length = prompt_context_length
+        self.node_feature_context_length = node_feature_context_length
         self.prompt_tokenizer = prompt_tokenizer
         self.graph_feature_tokenizer = graph_feature_tokenizer
+        
+        with open(self.config.graph_data_file, 'r') as f:
+            self.graph_data = []
+            for line in f:
+                self.graph_data.append(json.loads(line))
 
-        # Load annotations and validation set
-        with open(self.config.train_annotation_file, 'r') as f:
-            self.annotations = [json.loads(line) for line in f]
-
-        validation_set = pkl.load(open("data/gsm8k-validation.pkl", "rb"))
-        self.validation_questions = validation_set["question"]
-        self.validation_targets = validation_set["target"]
+        # Load validation set
+        if mode == "training":
+            validation_set = pkl.load(open(self.config.gsm8k_validation_file, "rb"))
+            self.prompts_data = validation_set["question"]
 
         self.pairs = self.create_pairs()
 
     def create_pairs(self):
         pairs = []
-        for annotation in self.annotations:
-            evaluations = annotation[1]["evaluations"]
-            for idx, eval_result in enumerate(evaluations):
-                if eval_result:  # Only consider successful evaluations
-                    question = self.validation_questions[annotation[1]["dataset_indexes"][idx]]
-                    graph = annotation[0]["graph"]
-                    pairs.append((graph, question))
+        for graph, evaluations in self.graph_data:
+            for i in range(len(evaluations['evaluations'])):
+                if evaluations['evaluations'][i]:  # Only consider successful evaluations
+                    prompt = self.prompts_data[evaluations['idx'][i]]
+                    pairs.append((self.process_graph(graph['graph']), self.tokenize_prompt(prompt)))
         return pairs
 
     def tokenize_prompt(self, text):
         tokens = self.prompt_tokenizer.encode(text).ids
-        result = torch.zeros(self.context_length, dtype=torch.long)
+        result = torch.zeros(self.prompt_context_length, dtype=torch.long)
         result[:len(tokens)] = torch.tensor(tokens)
         return result
 
@@ -53,7 +54,7 @@ class CLGP_Ebiose_dataset(Dataset):
         return graph_input, text_input
 
     def process_graph(self, graph):
-        graph_data = graph_struct_to_tokenized_graph_data(graph)
+        graph_data = self.parse_graph(graph)
         
         encoder_name = self.config.graph_encoder
         
@@ -70,47 +71,45 @@ class CLGP_Ebiose_dataset(Dataset):
 
         return graph_inputs
     
-    
-def graph_struct_to_tokenized_graph_data(graph_struct):
-    G = parse_graph(graph_struct)
+    def parse_graph(self, graph_struct):
+        # Extract node features
+        shared_context_prompt = graph_struct.get('shared_context_prompt','')
+        nodes = graph_struct.get("nodes", [])
+        edges = graph_struct.get("edges", [])
 
-    # Extract node features
-    node_features = np.array([G.nodes[i].get('features', [0, 0, 0]) for i in G.nodes])  # Default to [0, 0, 0] if no features
+        node_features = []
+        node_id_map = {}
+        for i, node in enumerate(nodes):
+            node_id_map[node["id"]] = i
+            node_features.append(['name:' + node.get('id', '') + '    purpose: ' + node.get('purpose', '') + '     type: ' + node.get('type','')+ '   model: ' + node.get('model','')+ '     shared_context_prompt: ' + shared_context_prompt])
 
-    # Convert to PyTorch tensors
-    x = torch.tensor(node_features, dtype=torch.float)
-    edge_index = torch.tensor(np.array(list(G.edges)).T, dtype=torch.long)
+        # Create edge index
+        edge_index = []
+        for edge in edges:
+            start_node = node_id_map[edge["start_node_id"]]
+            end_node = node_id_map[edge["end_node_id"]]
+            if edge.get('condition','') != '':
+                condition_node = ['name:' + edge['condition']+ '    purpose: ' + 'Allows acces to the next step if verified'+ '     type: ' + 'condition'+ '    model: ', '     shared_context_prompt: ']
+                node_features.append(condition_node)
+                edge_index.append([start_node, len(node_features)-1])
+                edge_index.append([len(node_features)-1, end_node])
+            else:
+                edge_index.append([start_node, end_node])
 
-    # Create a PyTorch Geometric data object
-    graph_data = Data(x=x, edge_index=edge_index)
-    
-    return graph_data
+        # Tokenize Node Feature
+        node_features = []
+        for features in node_features:
+            tokenized_features = torch.zeros((self.node_feature_context_length), dtype=torch.long)
+            tokens = self.graph_feature_tokenizer.encode(features).ids
+            tokenized_features[:len(tokens)] = tokens 
+            node_features.append(tokenized_features)
+        node_features = torch.stack(node_features)
+        
+        # Convert node features to tensor
+        edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
 
-def parse_graph(graph_struct):
-    # Extract node features
-    nodes = graph_struct.get("nodes", [])
-    edges = graph_struct.get("edges", [])
+        # Create a PyTorch Geometric data object
+        graph_data = Data(x=node_features, edge_index=edge_index)
+        
+        return graph_data
 
-    node_features = []
-    node_id_map = {}
-    for i, node in enumerate(nodes):
-        node_id_map[node["id"]] = i
-        node_features.append(node.get('features', [0, 0, 0]))  # Default to [0, 0, 0] if no features
-
-    # Convert node features to tensor
-    x = torch.tensor(node_features, dtype=torch.float)
-
-    # Create edge index
-    edge_index = []
-    for edge in edges:
-        start_node = node_id_map[edge["start_node_id"]]
-        end_node = node_id_map[edge["end_node_id"]]
-        edge_index.append([start_node, end_node])
-
-    # Convert edge index to tensor
-    edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
-
-    # Create a PyTorch Geometric data object
-    graph_data = Data(x=x, edge_index=edge_index)
-    
-    return graph_data
