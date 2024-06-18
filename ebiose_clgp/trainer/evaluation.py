24b24de46a2b828d9d739007ea4d69d8d1d6b4e3
EtiNL <etienne.lanzeray@gmail.com>
2024-06-19 00:03:14 +0200

    modified:   ebiose_clgp/trainer/bert_train_config.yaml 	modified:   ebiose_clgp/trainer/evaluation.py

--- ebiose_clgp/trainer/evaluation.py
+++ ebiose_clgp/trainer/evaluation.py
@@ -4,6 +4,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 import hashlib
 import wandb
+import os
+import pickle
 
 def hash_tensor(tensor):
     """Generate a hash for a given tensor."""
@@ -12,6 +14,14 @@ def hash_tensor(tensor):
 def unbatch_graphs(batch):
     data_list = batch.to_data_list()
     return data_list
+
+def save_embeddings_map(path, graph_embeddings_map, text_embeddings_map):
+    with open(path, 'wb') as f:
+        pickle.dump((graph_embeddings_map, text_embeddings_map), f)
+
+def load_embeddings_map(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
 
 def get_embeddings(model, dataloader, device):
     model.eval()
@@ -37,22 +47,28 @@ def get_embeddings(model, dataloader, device):
 
                 graph_embeddings_map[graph_hash] = graph_embedding.cpu().numpy()
                 text_embeddings_map[text_hash] = text_embedding.cpu().numpy()
-    
+
     return graph_embeddings_map, text_embeddings_map
 
-def evaluate_similarity(train_dataloader, test_dataloader, model, index_map, evaluation_map, hist_bins, device='cuda'):
+def evaluate_similarity(train_dataloader, test_dataloader, model, index_map, evaluation_map, hist_bins, device='cuda', saving_path=None):
     model = model.to(device)
     
-    train_graph_embeddings_map, train_text_embeddings_map = get_embeddings(model, train_dataloader, device)
-    test_graph_embeddings_map, test_text_embeddings_map = get_embeddings(model, test_dataloader, device)
+    if saving_path and os.path.exists(saving_path):
+        train_graph_embeddings_map, train_text_embeddings_map, test_graph_embeddings_map, test_text_embeddings_map = load_embeddings_map(saving_path)
+    else:
+        train_graph_embeddings_map, train_text_embeddings_map = get_embeddings(model, train_dataloader, device)
+        test_graph_embeddings_map, test_text_embeddings_map = get_embeddings(model, test_dataloader, device)
+        
+        if saving_path:
+            save_embeddings_map(saving_path, (train_graph_embeddings_map, train_text_embeddings_map, test_graph_embeddings_map, test_text_embeddings_map))
     
     train_graph_hashes = set(train_graph_embeddings_map.keys())
-    
+
     histogram_1 = []
     histogram_2 = []
     histogram_3 = []
     histogram_4 = []
-    
+
     for idx, (graph_hash, prompt_hash) in index_map.items():
         eval_score = evaluation_map[(graph_hash, prompt_hash)]
         if graph_hash in test_graph_embeddings_map and prompt_hash in test_text_embeddings_map:
@@ -73,12 +89,72 @@ def evaluate_similarity(train_dataloader, test_dataloader, model, index_map, eva
                 else:
                     print(4)
                     histogram_4.append(cosine_similarity([test_graph_embedding], [test_prompt_embedding])[0][0])
-    
+
     # Log histograms to wandb
-    wandb.log({"known graph association test, if eval = true": wandb.Histogram(np_histogram=np.histogram(histogram_1, bins= hist_bins))})
-    wandb.log({"known graph association test, if eval = false": wandb.Histogram(np_histogram=np.histogram(histogram_2, bins= hist_bins))})
-    wandb.log({"generation metric test, if eval = true": wandb.Histogram(np_histogram=np.histogram(histogram_3, bins= hist_bins))})
-    wandb.log({"generation metric test, if eval = false": wandb.Histogram(np_histogram=np.histogram(histogram_4, bins= hist_bins))})
-    
+    wandb.log({"known graph association test, if eval = true": wandb.Histogram(np_histogram=np.histogram(histogram_1, bins=hist_bins))})
+    wandb.log({"known graph association test, if eval = false": wandb.Histogram(np_histogram=np.histogram(histogram_2, bins=hist_bins))})
+    wandb.log({"generation metric test, if eval = true": wandb.Histogram(np_histogram=np.histogram(histogram_3, bins=hist_bins))})
+    wandb.log({"generation metric test, if eval = false": wandb.Histogram(np_histogram=np.histogram(histogram_4, bins=hist_bins))})
+
     return histogram_1, histogram_2, histogram_3, histogram_4
 
+if __name__ == "__main__":
+    from ebiose_clgp.trainer.utils import mkdir, load_config_file
+    from ebiose_clgp.data_utils.tokenizer import get_max_position_embedding
+    from omegaconf import OmegaConf
+    from ebiose_clgp.data_utils.dataloader import get_dataloader
+    from ebiose_clgp.data_utils.dataset import CLGP_Ebiose_dataset
+    from ebiose_clgp.model.CLGP import CLGP
+    from ebiose_clgp.trainer.train_utils import set_seed
+    from ebiose_clgp.trainer.evaluation import evaluate_similarity
+    from ebiose_clgp.model.text_encoders.bert import get_Bert
+    
+    DATA_CONFIG_PATH = 'Ebiose_CLGP/ebiose_clgp/data_utils/data_config.yaml'
+    TRAINER_CONFIG_PATH = 'Ebiose_CLGP/ebiose_clgp/trainer/bert_train_config.yaml'
+    MODEL_CONFIG_PATH = 'Ebiose_CLGP/ebiose_clgp/model/bert_model_config.yaml'
+    saving_path = "path/to/save/embeddings.pkl"
+    # Load your config, model, and dataloaders
+    data_config = load_config_file(DATA_CONFIG_PATH)
+    train_config = load_config_file(TRAINER_CONFIG_PATH)
+    model_config = load_config_file(MODEL_CONFIG_PATH)
+
+    config = OmegaConf.merge(train_config, data_config, model_config)
+
+    wandb.init(project='Ebiose_CLGP')
+
+    mkdir(path=config.saved_checkpoints)
+
+    config.device = "cuda" if torch.cuda.is_available() else "cpu"
+    config.n_gpu = torch.cuda.device_count()
+    
+    if config.text_encoder.name == 'Transformer':
+        config.graph_node_tokenizer_max_pos = get_max_position_embedding(config.graph_feature_tokenizer)
+        config.prompt_tokenizer_max_pos = get_max_position_embedding(config.prompt_tokenizer)
+        tokenizer = None
+        model = None
+        
+    elif config.text_encoder.name == 'Bert':
+        tokenizer, model = get_Bert()
+        config.embed_dim = 768 #Bert embedding dimension
+    
+    config.model_save_name = wandb.run.name
+    
+    set_seed(seed=11, n_gpu=config.n_gpu)
+    
+    model = CLGP(config, model)
+
+    # Load the model from the saved checkpoint
+    checkpoint = torch.load(config.saved_model)
+    if config.n_gpu > 1:
+        model.module.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+    dataset = CLGP_Ebiose_dataset(config, tokenizer=tokenizer)
+    train_dataset, val_dataset, test_dataset = dataset.train_validation_test_split()
+    
+    print("model evaluation...")
+    # Evaluate similarity and log histograms
+    train_dataloader = get_dataloader(config, train_dataset, is_train=False)
+    test_dataloader = get_dataloader(config, test_dataset, is_train=False)
+    evaluate_similarity(train_dataloader, test_dataloader, model, dataset.index_map, dataset.evaluation_map, config.eval_hist_bins, config.device, saving_path)
