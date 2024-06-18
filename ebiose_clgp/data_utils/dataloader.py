14efdadd098c8022e24687d3097e21c6ff2197fa
EtiNL <etienne.lanzeray@gmail.com>
2024-06-18 22:57:55 +0200

    modified:   ebiose_clgp/data_utils/dataloader.py

--- ebiose_clgp/data_utils/dataloader.py
+++ ebiose_clgp/data_utils/dataloader.py
@@ -3,6 +3,6 @@ from ebiose_clgp.data_utils.data_utils import collate_graph
 
 def get_dataloader(config, dataset, is_train=True):
     sampler = RandomSampler(dataset) if is_train else SequentialSampler(dataset)
-    batch_size = config.per_gpu_train_batch_size * max(1, config.n_gpu)
+    batch_size = config.train_batch_size * max(1, config.n_gpu) if is_train else config.eval_batch_size
     
     return DataLoader(dataset, sampler=sampler, batch_size=batch_size, num_workers=config.num_workers, collate_fn=collate_graph, pin_memory=True, prefetch_factor=2)
