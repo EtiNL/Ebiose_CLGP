d0dca19c51175bd72a455b4a351ed908ab92b17e
EtiNL <etienne.lanzeray@gmail.com>
2024-06-19 18:27:37 +0200

    modified:   ebiose_clgp/trainer/bert_train_config.yaml 	modified:   ebiose_clgp/trainer/evaluation.py

--- ebiose_clgp/trainer/evaluation.py
+++ ebiose_clgp/trainer/evaluation.py
@@ -50,6 +50,7 @@ def get_embeddings(model, dataloader, device):
             for graph_data, graph_embedding, text, text_embedding in zip(graph_data_list, graph_embeddings, texts_list, text_embeddings):
                 graph_tensor = graph_data.x.cpu()
                 print(graph_tensor.shape)
+                breakpoint()
                 graph_hash = hash_tensor(graph_tensor)
                 text_hash = hash_tensor(text.cpu())
 
