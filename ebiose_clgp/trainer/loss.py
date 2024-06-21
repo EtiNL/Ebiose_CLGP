023267c1e7d51abbb90857a7368288221998e077
EtiNL <etienne.lanzeray@gmail.com>
2024-06-21 13:36:33 +0200

    modified:   ebiose_clgp/trainer/loss.py

--- ebiose_clgp/trainer/loss.py
+++ ebiose_clgp/trainer/loss.py
@@ -12,10 +12,8 @@ class ContrastiveLoss(nn.Module):
         output1 = F.normalize(output1, p=2, dim=1)
         output2 = F.normalize(output2, p=2, dim=1)
 
-        # Calculate the cosine similarity
         cosine_similarity = F.cosine_similarity(output1, output2)
         
-        # The margin here is for pushing the cosine similarity of negative pairs towards -1
         loss_contrastive = torch.mean(
             (1 - label) * (cosine_similarity - self.margin).pow(2) +
             label * (1 - cosine_similarity).pow(2)
