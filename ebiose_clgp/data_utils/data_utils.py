8e9cdbe799ee7298706db5d8984dc0cb0611f7d8
EtiNL <etienne.lanzeray@gmail.com>
2024-07-07 17:16:47 +0200

    modified:   ebiose_clgp/data_utils/data_utils.py

--- ebiose_clgp/data_utils/data_utils.py
+++ ebiose_clgp/data_utils/data_utils.py
@@ -1,20 +1,6 @@
 import torch
 from torch_geometric.data import Batch, Data
 
-def pad_graphs(graphs):
-    max_nodes = max([graph[0].size(0) for graph in graphs])
-    padded_graphs = []
-
-    for node_features, edge_index in graphs:
-        pad_size = max_nodes - node_features.size(0)
-        if pad_size > 0:
-            pad_tensor = torch.zeros((pad_size, node_features.size(1)), dtype=node_features.dtype)
-            node_features = torch.cat([node_features, pad_tensor], dim=0)
-        
-        padded_graphs.append((node_features, edge_index))
-
-    return padded_graphs
-
 def collate_graph(batch):
     graphs, texts, labels = zip(*batch)
     
